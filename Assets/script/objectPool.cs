d33ef7df78ba7d56ad93e561dac11c4fcc3422b7
kevin <kevinzhuo1996@gmail.com>
2018-09-24 01:07:55 +0800

    basic function

---
+++ Assets/script/objectPool.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//add maximum amount
+public class objectPool : MonoBehaviour {
+
+    // Parameter
+   
+    [SerializeField]
+    private List<GameObject> inObjectList;
+    [SerializeField]
+    private List<GameObject> usingObjectList;
+    private bool reset;
+
+    public GameObject objectPrefab;
+    public Transform returnTransform;
+    
+    // Monobehavior
+
+    private void Start()
+    {
+        // Add the Children Objects to the inObjectList, which means the elements are unassigned.
+        GameObject go;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            go = transform.GetChild(i).gameObject;
+            inObjectList.Add(go);
+
+            if (go.GetComponent<Rigidbody>() != null)
+                go.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+    }
+
+    // public method
+    public GameObject GetObject()
+    {
+        GameObject obj;
+        if (inObjectList.Count == 0)
+        {
+            obj = Instantiate(objectPrefab, this.transform, false);
+            return obj;
+        }
+        else
+        {
+            obj = inObjectList[0];
+            usingObjectList.Add(obj);
+            inObjectList.RemoveAt(0);
+
+            return obj;
+        }
+    }
+
+    public void ReleaseObject(GameObject obj)
+    {
+        if (usingObjectList.Contains(obj))
+        {
+            obj.transform.position = this.transform.position;
+            inObjectList.Add(obj);
+            usingObjectList.Remove(obj);
+        }
+        else
+        {
+            Debug.Log("Object doesn't contain in Object Pool !");
+        }
+    }
+}
---
+++ Assets/script/shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//add Release() situation
+public class shoot : MonoBehaviour
+{
+    private GameObject go;
+    private objectPool op;
+
+    public Vector3 v;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        op = FindObjectOfType<objectPool>();
+    }
+
+    void shootObject()
+    {
+        go = op.GetObject();
+        go.transform.position = this.transform.position + offset;
+        go.GetComponent<Rigidbody>().isKinematic = false;
+        go.GetComponent<Rigidbody>().velocity = v;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+            shootObject();
+    }
+}
